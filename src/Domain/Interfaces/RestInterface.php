106f2b9f62c31a594ee6886fe0b22b7661dcc030
Edgar Gomez <edgar.gomez@yaydoo.com>
2023-10-01 01:41:52 -0600

    Solucion de bug RestInterface

--- src/Domain/Interfaces/RestInterface.php
+++ src/Domain/Interfaces/RestInterface.php
@@ -4,8 +4,6 @@ namespace Leadsales\GatewayBridge\Domain\Interfaces;
 
 interface RestInterface
 {
-    public function getUri(array $params, string $endpoint): string;
-    
     public function get(string $uri):mixed;
     
     public function post(string $uri, array $data):mixed;
