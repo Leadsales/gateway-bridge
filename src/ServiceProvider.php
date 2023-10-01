82da672a218198a37c9addd99826804603e28cb8
Edgar Gomez <edgar.gomez@yaydoo.com>
2023-10-01 01:25:42 -0600

    Solucion bug en service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -16,12 +16,6 @@ class ServiceProvider extends BaseServiceProvider
     public function boot(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/firebase.php', 'firebase');
-
-        $gatewayPath = $this->app['request']->server->get('GATEWAY_PATH');
-        $configPath = source_path($gatewayPath);
-        if (file_exists($configPath)) {
-            $this->mergeConfigFrom($configPath, 'gateway');
-        }
     }
 
     /**
