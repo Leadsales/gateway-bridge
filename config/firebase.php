03444b9d8766d1544d93232002e3138660f45ab0
Edgar Gomez <edgar.gomez@yaydoo.com>
2023-10-20 09:17:12 -0600

    Actualización firestore config

--- config/firebase.php
+++ config/firebase.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'sandbox' => env('FIREBASE_SANDBOX', false),
     /*
      * ------------------------------------------------------------------------
      * Default Firebase project
@@ -53,11 +52,11 @@ return [
                     "private_key" => env('FIREBASE_PRIVATE_KEY'),
                     "client_email" => env('FIREBASE_CLIENT_EMAIL'),
                     "client_id" => env('FIREBASE_CLIENT_ID'),
-                    "auth_uri" => env('FIREBASE_AUTH_URI'),
-                    "token_uri" => env('FIREBASE_TOKEN_URI'),
-                    "auth_provider_x509_cert_url" => env('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
-                    "client_x509_cert_url" => env('FIREBASE_CLIENT_X509_509'),
-                    "universe_domain" => env('FIREBASE_UNIVERSE_DOMAIN')
+                    "client_x509_cert_url" => env('FIREBASE_CLIENT_X509_CERT_URL'),
+                    "auth_uri" => env('FIREBASE_AUTH_URI', 'https://accounts.google.com/o/oauth2/auth'),
+                    "token_uri" => env('FIREBASE_TOKEN_URI', 'https://oauth2.googleapis.com/token'),
+                    "auth_provider_x509_cert_url" => env('FIREBASE_AUTH_PROVIDER_X509_CERT_URL', 'https://www.googleapis.com/oauth2/v1/certs'),
+                    "universe_domain" => env('FIREBASE_UNIVERSE_DOMAIN', 'googleapis.com')
                 ]),
             ],
 
